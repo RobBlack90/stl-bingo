710a5bb7710b00d1d9e3bbc6d056ad3d9e4e5e0d
blackr <RobBlack90@gmail.com>
2020-03-21 13:14:42 -0500

    Real tawk

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,9 +8,20 @@
           <img class="w-full md:w-4/6" alt="STL Bingo!" src="../assets/images/STLBingo.png">
           <p class="text-lg">#STLBingo</p>
         </div>
-        <p class="pb-10">
-          I'm baby vaporware cardigan ethical quinoa church-key tumblr taxidermy neutra intelligentsia artisan asymmetrical cliche. Celiac ethical venmo, cronut PBR&B salvia copper mug fam unicorn etsy fashion axe flexitarian irony vice. Chia lyft fingerstache tousled salvia unicorn banjo craft beer YOLO narwhal chambray lomo PBR&B bicycle rights. Hell of raclette polaroid church-key.
-        </p>
+        <div class="pb-10">
+          <p class="my-4">
+            It's a strange, socially distant time we're livin' in, and we all want to do what you can to help out our favorite STL small businesses! I've created a bingo card to make sure you spread the love! Generate a custom bingo card filled with a randomized selection of this city's best local food and/or drink establishments currently offering takeout or delivery. Take a screenshot of your card, post to your favorite social media site (using the hashtag #STLBingo), and mark the spots you've supported!  
+          </p>
+          <p class="my-4">
+            <span class="italic font-semibold">What do you win if you get a bingo?</span> Currently- the satisfaction of helping out some local spots!
+          </p>
+          <p class="my-4">
+            <span class="italic font-semibold">Why is there no free space?</span> C'mon son! No freebies here! Help out your neighbor!
+          </p>
+          <p class="my-4">
+            <span class="italic font-semibold">Did I miss an establishment?</span> I probably did. Shoot me an email (RobBlack90@gmail.com) or Instagram message <a class="underline" href="https://www.instagram.com/thatrobbyblack" target="_blank">@ThatRobbyBlack</a> and I'll get it added!    
+          </p>
+        </div>
         <div class="flex flex-col justify-center items-center">
           <el-dropdown class="p-5">
             <span class="el-dropdown-link">
@@ -72,4 +83,15 @@ export default {
   color: #ffffff;
   font-family: Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
 }
+  a {
+    color: #ffffff;
+    text-decoration: none;
+    transition: color 0.3s;
+  }
+
+  a:hover {
+    color: #edf2f7;
+    text-decoration:none; 
+    cursor:pointer;  
+  }
 </style>
