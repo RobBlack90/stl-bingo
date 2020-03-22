6851bf2f5db495fa822f6734d31ad5b859a12d49
blackr <RobBlack90@gmail.com>
2020-03-22 14:24:44 -0500

    gtag

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import ElementUI from 'element-ui';
 import locale from 'element-ui/lib/locale/lang/en'
 import './assets/css/tailwind.css';
 import router from './router'
+import VueGtag from "vue-gtag";
+
+Vue.use(VueGtag, {
+  config: { id: "UA-161613753-1" }
+}, router)
 
 Vue.use(ElementUI, { locale })
 
